55f4555c75bf524051c6f5e0a4a945a645cc5699
Marcelo Santos <marcelo.b.g.santos@gmail.com>
2023-05-29 21:55:22 +0100

    done

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ironhackLogo from './images/ironhack-logo-xs.png'
 import menuTop from './images/menu-top-xs.png'
 import icon1 from './images/icon1.png'
 import icon2 from './images/icon2.png'
 import icon3 from './images/icon3.png'
 import icon4 from './images/icon4.png'
 
+/*
 const ironhackLogo = 'https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png';
+*/
 
 function App() {
   return (
